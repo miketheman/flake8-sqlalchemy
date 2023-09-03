27ec3070391d24464784adfda99c5bf86b48ba4b
Mike Fiedler <miketheman@gmail.com>
2023-09-03 18:38:04 -0400

    fix: fall through if we don't detect the call
    Fixes #13
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- flake8_sqlalchemy/checkers/_base.py
+++ flake8_sqlalchemy/checkers/_base.py
@@ -1,18 +1,17 @@
 import ast
-from typing import Any, List
+from typing import Any, List, Optional
 
 from ..issue import Issue
 
 
 class Checker:
     @staticmethod
-    def get_call_name(node: ast.Call) -> str:
+    def get_call_name(node: ast.Call) -> Optional[str]:
         if isinstance(node.func, ast.Attribute):
             return node.func.attr
         if isinstance(node.func, ast.Name):
             return node.func.id
-        # Fallthrough for any unknown conditions. Eventually remove this.
-        raise Exception(f"Unknown node.func type for {node.func}")  # pragma: no cover
+        return None
 
     def run(self, node: Any) -> List[Issue]:  # TODO correct type hint
         raise NotImplementedError
