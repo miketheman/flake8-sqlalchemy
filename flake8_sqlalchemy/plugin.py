dd7657335205027d02f99b0a11a578459f5b1e2f
Mike Fiedler <miketheman@gmail.com>
2024-06-15 10:23:43 -0400

    Add checker for relationship backref usage

--- flake8_sqlalchemy/plugin.py
+++ flake8_sqlalchemy/plugin.py
@@ -2,7 +2,7 @@ import ast
 import importlib.metadata
 from typing import Any, Dict, Generator, List, Tuple, Type
 
-from .checkers import ColumnCommentChecker, ImportAliasChecker
+from .checkers import ColumnCommentChecker, ImportAliasChecker, RelationshipBackrefChecker
 from .issue import Issue
 
 
@@ -10,6 +10,7 @@ class Visitor(ast.NodeVisitor):
     checkers: Dict[str, List[Any]] = {
         "Call": [
             ColumnCommentChecker(),
+            RelationshipBackrefChecker(),
         ],
         "Import": [
             ImportAliasChecker(),
